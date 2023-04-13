a2be88523ff105568808b1fefe126334df376e27
JCeginner <97357328+JCeginner@users.noreply.github.com>
2023-04-13 10:46:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cudtm3qsydfd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jennifer-employee"
+customregion = "us-east-1"
